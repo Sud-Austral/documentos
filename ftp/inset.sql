094daf08b1176d40dceecc955f039ba92677a36c
lmonsalve <lmonsalve22@gmail.com>
2020-05-21 13:13:18 -0400

    commit

---
+++ ftp/inset.sql
@@ -0,0 +1,150 @@
+select * from tablabibliogra order by id_texto desc;
+
+
+insert into tablabibliogra values (
+        'H1',
+        'Actores Consolidados',
+        'MARN MAGA INAB CONAP',
+        null,
+        2020,
+        null,
+        null,
+        null,
+        null,
+        null,
+        9,
+        24,
+        1,
+        2,
+        'actor consolidado'
+        );
+
+insert into tablabibliogra values (
+        'H2',
+        'Análisis de Contexto de Territorios y Mapeo de actores locales para la construcción participativa de la ENDDBG',
+        'MARN MAGA INAB CONAP',
+        null,
+        2020,
+        null,
+        null,
+        null,
+        null,
+        null,
+        9,
+        24,
+        1,
+        2,
+        'análisis contexto territorios mapeo actor local construcción participativa ENDDBG'
+        );
+        
+insert into tablabibliogra values (
+        'H3',
+        'Plan General de Diálogo y Participación para la construcción participativa de la ENDDBG',
+        'MARN MAGA INAB CONAP',
+        null,
+        2020,
+        null,
+        null,
+        null,
+        null,
+        null,
+        9,
+        24,
+        1,
+        2,
+        'plan general diálogo participación construcción participativa ENDDBG'
+        );
+        
+        
+insert into tablabibliogra values (
+        'H4',
+        'Planes Territoriales de Diálogo y Participación para la construcción participativa de la ENDDBG',
+        'MARN MAGA INAB CONAP',
+        null,
+        2020,
+        null,
+        null,
+        null,
+        null,
+        null,
+        9,
+        24,
+        1,
+        2,
+        'planes territoriales diálogo participación construcción participativa ENDDBG'
+        );
+        
+        
+insert into tablabibliogra values (
+        'H5',
+        'Estrategia de Socialización, Difusión y Comunicación para la construcción participativa',
+        'MARN MAGA INAB CONAP',
+        null,
+        2020,
+        null,
+        null,
+        null,
+        null,
+        null,
+        9,
+        24,
+        1,
+        2,
+        'estrategia socialización difusión comunicación construcción participativa estrategia nacional reducción deforestación degradación bosques Guatemala ENDDBG'
+        );
+        
+        
+        
+insert into tablabibliogra values (
+        'H6',
+        'Directrices para el involucramiento de las partes relevantes en la preparación para REDD+',
+        'MARN MAGA INAB CONAP',
+        null,
+        2020,
+        null,
+        null,
+        null,
+        null,
+        null,
+        9,
+        24,
+        1,
+        2,
+        'directrices involucramiento parte relevante preparación REDD+ pueblo indígena bosque'
+        );
+        
+insert into tablabibliogra values (
+        'H7',
+        'Lista de Talleres',
+        'MARN MAGA INAB CONAP',
+        null,
+        2020,
+        null,
+        null,
+        null,
+        null,
+        null,
+        9,
+        24,
+        1,
+        2,
+        'lista taller'
+        );
+        
+insert into tablabibliogra values (
+        'H8',
+        'Ruta de Trabajo para la Incorporación de Consideraciones de Género',
+        'MARN MAGA INAB CONAP',
+        null,
+        2020,
+        null,
+        null,
+        null,
+        null,
+        null,
+        9,
+        24,
+        1,
+        2,
+        'ruta trabajo incorporación consideracion género'
+        );
--- inset.sql
+++ inset.sql
@@ -5,7 +5,7 @@ insert into tablabibliogra values (
         'H1',
         'Actores Consolidados',
         'MARN MAGA INAB CONAP',
-        null,
+        'actor consolidado fortalecimiento participacion desarrollo mecanismos monitoreo departamental acesoría guatemala ambiental conap guatemala gobierno central nuevoenddbg',
         2020,
         null,
         null,
@@ -16,16 +16,25 @@ insert into tablabibliogra values (
         24,
         1,
         2,
-        'actor consolidado'
+        'actor consolidado fortalecimiento participacion desarrollo mecanismos monitoreo departamental acesoría guatemala ambiental conap guatemala gobierno central nuevoenddbg'
         );
 
 insert into tablabibliogra values (
         'H2',
         'Análisis de Contexto de Territorios y Mapeo de actores locales para la construcción participativa de la ENDDBG',
-        'MARN MAGA INAB CONAP',
-        null,
+        'INDUFOR OY',
+        'análisis contexto territorios mapeo actor local construcción participativa contexto general enredd+ desarrollo atun sistema de consejos incentivos forestales peten motagua corredor seco coban ixcan',
         2020,
-        null,
+        'El presente análisis hace un recorrido por cinco territorios 
+        (divididos por subterritorios) potencialmente priorizados para 
+        actividades REDD+ en Guatemala, describiendo el contexto sociocultural 
+        y dinámicas relacionadas con las tres medidas REDD+; identificando partes 
+        interesadas y plataformas de gobernanza forestal de consulta; y analizando 
+        roles de género y el alcance de participación de las mujeres en el manejo 
+        de los recursos forestales. Además, se incluye en el documento una primera 
+        propuesta de las plataformas en los territorios que pueden servir como canales 
+        de divulgación, socialización, acompañamiento y convocatoria de la consulta de 
+        la Estrategia Nacional REDD+ (ENREDD+).',
         null,
         null,
         null,
@@ -34,16 +43,24 @@ insert into tablabibliogra values (
         24,
         1,
         2,
-        'análisis contexto territorios mapeo actor local construcción participativa ENDDBG'
+        'análisis contexto territorios mapeo actor local construcción participativa contexto general enredd+ desarrollo atun sistema de consejos incentivos forestales peten motagua corredor seco coban ixcan'
         );
         
 insert into tablabibliogra values (
         'H3',
         'Plan General de Diálogo y Participación para la construcción participativa de la ENDDBG',
-        'MARN MAGA INAB CONAP',
-        null,
+        'INDUFOR OY',
+        'plan general diálogo participación construcción participativa cambio climático plataformas codena dialogo participacion consulta socializacion sistematizacion alimentacion',
         2020,
-        null,
+        'Mediante el Plan General de Diálogo y Participación se establecen los lineamientos
+         y metodologías que pueden orientar a las instancias públicas y partes interesadas a
+        construir las capacidades y mecanismos para garantizar la participación plena y efectiva
+        durante la elaboración de la Estrategia Nacional de REDD+ (ENREDD+) de Guatemala. La 
+        implementación de dicho Plan se concretiza a través de Planes Territoriales de Diálogo
+        y Participación que se construirán con las partes interesadas y plataformas regionales,
+        reflejando la representación y dinámica particulares de cada región. Por tanto, el 
+        presente Plan “madre” describe el proceso metodológico general que luego se adaptará 
+        en cada territorio priorizado a través de sus planes propios.',
         null,
         null,
         null,
@@ -52,17 +69,23 @@ insert into tablabibliogra values (
         24,
         1,
         2,
-        'plan general diálogo participación construcción participativa ENDDBG'
+        'plan general diálogo participación construcción participativa cambio climático plataformas codena dialogo participacion consulta socializacion sistematizacion alimentacion fuentes financieros cronograma mecanismos retroalimentacion acuerdos pueblos indigenas'
         );
         
         
 insert into tablabibliogra values (
         'H4',
         'Planes Territoriales de Diálogo y Participación para la construcción participativa de la ENDDBG',
-        'MARN MAGA INAB CONAP',
-        null,
+        'INDUFOR OY',
+        'planes territoriales diálogo participación construcción participativa ENDDBG participacion diálogo izabal resultado partes interesados proceso salvaguardas canal financiamiento comunicaciones monitoreo devoluciones acceso informacion petén',
         2020,
-        null,
+        'El propósito principal del proceso de diálogo y participación es llegar a una Estrategia
+        Nacional REDD+ que refleje las visiones de las partes interesadas, contribuyendo al pleno 
+        ejercicio de derechos y un desarrollo equitativo basado en las tres medidas REDD+. 
+        El diálogo es un requisito previo para que el país pueda obtener la aprobación de su 
+        Estrategia de parte del Fondo Cooperativo para el Carbono Forestal (FCPF), y para que
+        pueda acceder a los pagos por resultados de mecanismos de financiamiento del clima como
+        el Fondo de Carbono del FCPF y el Fondo Verde del Clima (Green Climate Fund), entre otros.',
         null,
         null,
         null,
@@ -71,17 +94,22 @@ insert into tablabibliogra values (
         24,
         1,
         2,
-        'planes territoriales diálogo participación construcción participativa ENDDBG'
+        'planes territoriales diálogo participación construcción participativa ENDDBG participacion diálogo izabal resultado partes interesados proceso salvaguardas canal financiamiento comunicaciones monitoreo devoluciones acceso informacion petén'
         );
         
-        
+delete from tablabibliogra where id_texto = 'H8';
+    
 insert into tablabibliogra values (
         'H5',
         'Estrategia de Socialización, Difusión y Comunicación para la construcción participativa',
         'MARN MAGA INAB CONAP',
-        null,
+        'estrategia socialización difusión comunicación construcción participativa estrategia nacional reducción deforestación degradación bosques Guatemala',
         2020,
-        null,
+        'La presente Estrategia de Socialización, Difusión y Comunicación para el Proceso de Diálogo y
+	Participación de la Estrategia Nacional para la Reducción de Emisiones por Deforestación Evitada y
+	Degradación de Bosques (ENREDD+) pretende promover una participación plena y efectiva, a través
+	del desarrollo de una serie de materiales de comunicación que permitan la difusión de la información
+	entre los distintos grupos identifiados como partes interesadas de la Estrategia.',
         null,
         null,
         null,
@@ -90,18 +118,24 @@ insert into tablabibliogra values (
         24,
         1,
         2,
-        'estrategia socialización difusión comunicación construcción participativa estrategia nacional reducción deforestación degradación bosques Guatemala ENDDBG'
-        );
+        'estrategia socialización difusión comunicación construcción participativa estrategia nacional reducción deforestación degradación bosques Guatemala ENDDBG socialización difución comunicacion diagnostico deforestación degradacion pertenencia apropación relaciones innovación difucion táctica medios' 
+	);
         
-        
+ select * from tablabibliogra where id_texto = 'H8';       
         
 insert into tablabibliogra values (
         'H6',
         'Directrices para el involucramiento de las partes relevantes en la preparación para REDD+',
         'MARN MAGA INAB CONAP',
-        null,
+        'directrices involucramiento parte relevante preparación REDD+ pueblo indígena bosque indigenas',
         2020,
-        null,
+        'Este documento establece directrices sobre los elementos clave para lograr la participación efectiva de
+	diversos actores en el contexto de preparación para REDD+, lo que incluye el Fondo para reducir las
+	emisiones de carbono mediante la protección de los bosques (FCPF) y el Programa ONU-REDD, con un
+	énfasis en el involucramiento de los pueblos indígenas y otras comunidades dependientes de los bosques;
+	en éste, se definen 1) los principios para una participación y consulta efectiva; 2) las directrices
+	operativas; y 3) directrices prácticas sobre cómo planificar y poner en marcha las actividades de
+	consulta',
         null,
         null,
         null,
@@ -110,16 +144,16 @@ insert into tablabibliogra values (
         24,
         1,
         2,
-        'directrices involucramiento parte relevante preparación REDD+ pueblo indígena bosque'
+        'directrices involucramiento parte relevante preparación REDD+ pueblo indígena bosque indigenas principios directrices consulta mecanismo esquema guiars métodos declaración finales sesa esmf programa onu representación transparencia rendición mecanismo'
         );
         
 insert into tablabibliogra values (
         'H7',
         'Lista de Talleres',
         'MARN MAGA INAB CONAP',
-        null,
+        'lista taller lingüística planificación degradación socialización congreso evaluación estratégia',
         2020,
-        null,
+        'Numeración de talleres',
         null,
         null,
         null,
@@ -128,14 +162,14 @@ insert into tablabibliogra values (
         24,
         1,
         2,
-        'lista taller'
+        'lista taller lingüística planificación degradación socialización congreso evaluación estratégia pueblo publico privao indigenas mancomunidad social ambiental foro encuentro territorial'
         );
         
 insert into tablabibliogra values (
         'H8',
         'Ruta de Trabajo para la Incorporación de Consideraciones de Género',
         'MARN MAGA INAB CONAP',
-        null,
+        'ruta trabajo incorporación consideracion género legar',
         2020,
         null,
         null,
@@ -146,5 +180,5 @@ insert into tablabibliogra values (
         24,
         1,
         2,
-        'ruta trabajo incorporación consideracion género'
+        'ruta trabajo incorporación consideracion género legar internacional cambio climatico proceso género mecanismos ruta incorporación estrategia acciones matriz preparación niveles estructura recuersos forestales'
         );
